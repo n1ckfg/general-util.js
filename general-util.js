6032da5e0aedb4c0e4499871c54cab50dd96bad5
Nick Fox-Gieg <nick@fox-gieg.com>
2020-10-31 20:56:12 -0400

    Update general-util.js

--- general-util.js
+++ general-util.js
@@ -120,6 +120,33 @@ class Util {
         return "";
     }
 
+    saveAsImage() {
+        let imgData, imgNode;
+        let strMime = "image/jpeg";
+        let strDownloadMime = "image/octet-stream";
+
+        try {
+            imgData = renderer.domElement.toDataURL(strMime);
+            saveFile(imgData.replace(strMime, strDownloadMime), "test.jpg");
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+    }
+
+    saveFile(strData, filename) {
+        let link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            document.body.appendChild(link); //Firefox requires the link to be in the body
+            link.download = filename;
+            link.href = strData;
+            link.click();
+            document.body.removeChild(link); //remove the link when done
+        } else {
+            location.replace(uri);
+        }
+    }
+
 }
 
 const util = new Util();
